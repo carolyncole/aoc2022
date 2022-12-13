373c05fb657ccc00bdbdf8b3856bc83c5c686ee1
Carolyn Cole <cac9@princeton.edu>
2022-12-13 15:43:08 -0500

    Updated to end with a true when a value is less Still not the correct answer

--- day13/script.rb
+++ day13/script.rb
@@ -4,28 +4,40 @@ require 'json'
 def correct_order?(pair_str)
   pair_strs = pair_str.split("\n")
   pairs = pair_strs.map{|pair| JSON.parse pair}
-  correct =  pair_in_correct_order(pairs[0],pairs[1])
-  puts correct
-  correct
+  puts "-------"
+  result =  pair_in_correct_order(pairs[0],pairs[1])
+  puts "result: #{result}"
+  puts "-------"
+  result[:result]
 end
 
 def pair_in_correct_order(part1, part2)
-  return part1 <= part2 if part1.is_a?(Integer) && part2.is_a?(Integer)
-  return false if part1.is_a?(Array) && part2.is_a?(Array) && part1.length > part2.length
-  return true if part2.nil?
+  puts "Compare #{part1} vs #{part2}" 
+  if part1.is_a?(Integer) && part2.is_a?(Integer)
+    puts "Right side is smaller, so inputs are not in the right order" if part2 < part1
+    return {continue: part1 == part2, result: part1 <= part2}
+  end
+  if part2.nil?
+    puts "Right side ran out of items, so inputs are not in the right order"
+    return {continue: false, result: false}
+  end
+  if part1.nil?
+    puts "Left side ran out of items, so inputs are in the right order"
+    return {continue: false, result: true}
+  end
 
   part1 = Array(part1)
   part2 = Array(part2)
   part1.each_with_index do |value1, index|
     value2 = part2[index]
-    correct = pair_in_correct_order(value1, value2)
-    return false unless correct
+    result = pair_in_correct_order(value1, value2)
+    return result unless result[:continue]
   end
-  true
+  {continue: true, result: true}
 end
 
-pairs = File.read("example.txt").split("\n\n")
-correct_order = pairs.map{|pair| correct_order?(pair) }
+pairs = File.read("input.txt").split("\n\n")
+correct_order = pairs.map.with_index{|pair, index| correct_order?(pair) }
 
 sum_indicies=0
 correct_order.each_with_index do |correct, index|
